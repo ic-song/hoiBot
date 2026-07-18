9e600aa07d332998e5760269b8ac6381c2ab3681
jinminy2 <jinminy2@gmail.com>
2026-07-19 01:35:06 +0900

    영지전 캐슬매력 판정 속도 개선

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 버전
 Device.acquireWakeLock(android.os.PowerManager.PARTIAL_WAKE_LOCK, "봇");
-const HoiBotVersion = "2.276"; // 수정 시 0.001 단위 증가
+const HoiBotVersion = "2.277"; // 수정 시 0.001 단위 증가
 let isDebuggerFlag = false; //
 let userState = {}; // 유저 상태 저장용
 let termsState = {}; // 약관 동의 상태 저장용
@@ -27940,9 +27940,9 @@ function resolveGuildTerritoryAttack(data, petData, guildData, petSkillData, sen
 
     var defenderExp = war.castleExpSnapshots[defenderName];
     var attackerExp = war.castleExpSnapshots[sender];
-    var gameResult = petgameplay(defenderExp, attackerExp, null);
+    var isAttackerWin = attackerExp > defenderExp; // 공격자 캐슬매력이 방어자보다 높을 때만 승리
 
-    if (gameResult.winner == 2) {
+    if (isAttackerWin) {
         //공격자 승리
         ter.ownerGuildId = attackerGuildInfo.guildId;
         ter.ownerUser = sender;
