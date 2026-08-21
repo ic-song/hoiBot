d20953168a66446edaf53dc72ecefde682026c2b
jinminy2 <jinminy2@gmail.com>
2026-08-21 17:29:14 +0900

    마스터 미니펫 운영 버전 반영

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // 버전
 Device.acquireWakeLock(android.os.PowerManager.PARTIAL_WAKE_LOCK, "봇");
-const HoiBotVersion = "2.397"; // 수정 시 0.001 단위 증가
+const HoiBotVersion = "2.398"; // 수정 시 0.001 단위 증가
 let isDebuggerFlag = false; //
 let userState = {}; // 유저 상태 저장용
 let termsState = {}; // 약관 동의 상태 저장용
